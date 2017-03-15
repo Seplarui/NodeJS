03dcf68dd68c306a7672c1325da365e36964e913
Sebas Sebas <seplarui@gmail.com>
2017-03-15 21:10:15 +0100

    Foo.js

--- Ejemplo1/main.js
+++ Ejemplo1/main.js
@@ -1,8 +1,7 @@
 var http=require('http');
-http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('Curso de Node.js\n');
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    res.end('Hola Mundo\n');
 }).listen(3000,"127.0.0.1");
-console.log('Server running at http://127.0.0.1:3000');
-});
+console.log('Server running at http://127.0.0.1:3000/');
 
